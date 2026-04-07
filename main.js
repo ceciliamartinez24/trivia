0aaa9a262972ceb1ff5610ab526f301a3f81065b
Cecilia Martinez <cecimilimart@gmail.com>
2026-04-06 23:00:47 -0300

    falta que cambie la pregunta

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 let preguntasTodas = []
 let categoriaSelec
 let nivelSelec 
+let preguntasJuego = []
+let indiceActual = 0
+let correctas = 0
+let incorrectas = 0
+let noRespondidas = 0
+
 
 fetch("trivia.json")
 .then(response => response.json())
@@ -13,7 +19,6 @@ fetch("trivia.json")
 })
 
 function mostrarCategorias (){
-
     const contenedorCat = document.getElementById("categorias")
     contenedorCat.innerHTML = ""
 
@@ -23,11 +28,11 @@ function mostrarCategorias (){
 
         boton.addEventListener("click",() => {
         categoriaSelec = cat 
+        document.getElementById("inicio").style.display = "none"
         mostrarNiveles()
     })
     contenedorCat.appendChild(boton)
     })
-    document.getElementById("dificultad").style.display = "block"
 }
 
 function mostrarNiveles(){
@@ -37,25 +42,81 @@ function mostrarNiveles(){
     const niveles = ["facil","medio","dificil"]
     
     niveles.forEach(nivel=> {
-        const boton = document.createElement("button")
-        boton.textContent = nivel
+        const botonNiveles = document.createElement("button")
+        botonNiveles.textContent = nivel
 
-        boton.addEventListener("click",() => {
+        botonNiveles.addEventListener("click",() => {
             nivelSelec = nivel
             iniciarJuego()
         })
-        contenedorDific.appendChild(boton)
+        contenedorDific.appendChild(botonNiveles)
     })
+    document.getElementById("dificultad").style.display = "block"
 }
 
 function iniciarJuego (){
-    document.getElementById("inicio").style.display = "none"
+    document.getElementById("dificultad").style.display = "none"
     document.getElementById("juego").style.display = "block"
 
     const preguntas = categoriaSelec.preguntas
 
     const preguntasMezcladas = preguntas.sort(()=> Math.random() -0.5)
-    const preguntasJuego = preguntasMezcladas.slice(0,5)
-console.log(preguntasJuego)
+    preguntasJuego = preguntasMezcladas.slice(0,5)
+    indiceActual = 0
+    mostrarPreguntas()
 }
 
+function mostrarPreguntas (){
+    const preguntaActual = preguntasJuego[indiceActual]
+    document.getElementById("pregunta").textContent = preguntaActual.pregunta
+    
+    const opciones = [
+        preguntaActual.correcta,
+        ...preguntaActual.incorrectas
+    ]
+
+    const opcionesMezcladas = opciones.sort(() => Math.random() -0.5)
+
+    const contenedorOpc = document.getElementById("opciones")
+        contenedorOpc.innerHTML = ""
+    
+    opcionesMezcladas.forEach(op=>{
+        const botonOpciones = document.createElement("button")
+        botonOpciones.textContent = op
+
+        botonOpciones.addEventListener("click", ()=>{
+            const esCorrecta = op === preguntaActual.correcta
+
+            if (esCorrecta){
+                alert("Correcto! =D")
+                correctas++
+            } else {
+                alert("incorrecta =( ")
+                incorrectas++
+            }
+        })
+        contenedorOpc.appendChild(botonOpciones)
+    })
+
+    indiceActual++
+    siguientePregunta()
+}
+
+function siguientePregunta() {
+
+    if (indiceActual < preguntasJuego.length){
+        mostrarPreguntas()
+    } else {
+        mostrarResultados()
+    }
+}
+
+function mostrarResultados() {
+    //document.getElementById("juego").style.display = "none"
+    document.getElementById("resultados").style.display = "block"
+
+    document.getElementById("correctas").textContent = "Correctas: " + correctas
+    document.getElementById("incorrectas").textContent = "Incorrectas: " + incorrectas
+    document.getElementById("noRespondidas").textContent = "No respondidas: " + noRespondidas
+
+}
